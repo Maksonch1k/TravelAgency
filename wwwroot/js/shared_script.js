38ae2e0b8a72c8ca6e0cfb6b54d5e690f4bf047e
olia315 <122738326+olia315@users.noreply.github.com>
2024-11-11 15:16:00 +0200

    Добавьте файлы проекта.

---
+++ wwwroot/js/shared_script.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        var header = document.getElementById('header-top');
+        var scroll = window.pageYOffset;
+        var maxScroll = 250; // Максимальная прокрутка для изменения цвета
+
+        // Прозрачность хедера
+        var opacity = Math.min(scroll / maxScroll, 1);
+        header.style.backgroundColor = `rgba(255, 100, 0, ${opacity})`;
+
+    });
+});
+
